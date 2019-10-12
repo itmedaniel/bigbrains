4a906826347f001b77f4e51ff5bf5f06cee83609
dangzhengyang <dangzhengyang@caacetc.com>
2019-10-12 11:05:05 +0800

    build Accounts

--- src/main/java/dangzhengyang/bigbrains/AccountsStatistics.java
+++ src/main/java/dangzhengyang/bigbrains/AccountsStatistics.java
@@ -16,9 +16,9 @@ public class AccountsStatistics {
     }
 
     public double statisticsTotalIncomeBy(List<AccountItem> accountItems, String date) {
-        List<AccountItem> accountItemByMonth = statisticsAccountsBy(accountItems, date);
+        List<AccountItem> accountItemByDate = statisticsAccountsBy(accountItems, date);
         double totalIncome = 0.0;
-        for (AccountItem accountItem: accountItemByMonth) {
+        for (AccountItem accountItem: accountItemByDate) {
             if (accountItem.getCategory().equals("Income")) {
                 totalIncome += accountItem.getAmount();
             }
@@ -27,9 +27,9 @@ public class AccountsStatistics {
     }
 
     public double statisticsTotalSpendingBy(List<AccountItem> accountItems, String date) {
-        List<AccountItem> accountItemByMonth = statisticsAccountsBy(accountItems, date);
+        List<AccountItem> accountItemByDate = statisticsAccountsBy(accountItems, date);
         double totalSpending = 0.0;
-        for (AccountItem accountItem: accountItemByMonth) {
+        for (AccountItem accountItem: accountItemByDate) {
             if (accountItem.getCategory().equals("spending")) {
                 totalSpending += accountItem.getAmount();
             }
@@ -38,12 +38,12 @@ public class AccountsStatistics {
     }
 
     public List<AccountItem> statisticsAccountsBy(List<AccountItem> accountItems, String date) {
-        List<AccountItem> accountItemByMonth = new ArrayList<AccountItem>();
+        List<AccountItem> accountItemByDate = new ArrayList<AccountItem>();
         for (AccountItem accountItem:accountItems) {
-            if (accountItem.getOccuredTime() == date) {
-                accountItemByMonth.add(accountItem);
+            if (accountItem.getOccuredTime().substring(0, date.length()).equals(date)) {
+                accountItemByDate.add(accountItem);
             }
         }
-        return accountItemByMonth;
+        return accountItemByDate;
     }
 }
--- src/test/java/dangzhengyang/bigbrains/AccountsStatisticsTest.java
+++ src/test/java/dangzhengyang/bigbrains/AccountsStatisticsTest.java
@@ -1,13 +1,13 @@
 package dangzhengyang.bigbrains;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.*;
-
 
 public class AccountsStatisticsTest {
 
-    public void should_statistics_profit_by_date() {
+    public List<AccountItem> initAccountItems() {
         List<AccountItem> accountItems = new ArrayList<AccountItem>();
         AccountItem accountItem1 = new AccountItem("打印", "spending", "试卷打印","", 1.3, "20190316");
         AccountItem accountItem2 = new AccountItem("用餐", "spending", "食堂用餐","", 0.4, "20190212");
@@ -17,14 +17,55 @@ public class AccountsStatisticsTest {
         AccountItem accountItem6 = new AccountItem("打印", "spending", "试卷打印","", 1.6, "20190216");
         AccountItem accountItem7 = new AccountItem("工资", "income", "工资收入","", 33.6, "20190320");
         accountItems.add(accountItem1);
+        accountItems.add(accountItem1);
         accountItems.add(accountItem2);
         accountItems.add(accountItem3);
         accountItems.add(accountItem4);
         accountItems.add(accountItem5);
         accountItems.add(accountItem6);
         accountItems.add(accountItem7);
-
-        double totalProfit = AccountsStatistics
+        return accountItems;
     }
+
+//    @Ignore
+    @Test
+    public void should_statistics_profit_by_date() {
+
+        List<AccountItem> accountItems = initAccountItems();
+        AccountsStatistics accountsStatistics = new AccountsStatistics();
+        double totalProfit = accountsStatistics.statisticsProfitBy(accountItems, "201902");
+        System.out.println(totalProfit);
+    }
+
+//    @Ignore
+    @Test
+    public void should_statistics_total_income_by_date() {
+
+        List<AccountItem> accountItems = initAccountItems();
+        AccountsStatistics accountsStatistics = new AccountsStatistics();
+        double totalIncome = accountsStatistics.statisticsTotalIncomeBy(accountItems, "201902");
+        System.out.println(totalIncome);
+    }
+
+//    @Ignore
+    @Test
+    public void should_statistics_total_spending_by_date() {
+
+        List<AccountItem> accountItems = initAccountItems();
+        AccountsStatistics accountsStatistics = new AccountsStatistics();
+        double totalSpending = accountsStatistics.statisticsTotalSpendingBy(accountItems, "201902");
+        System.out.println(totalSpending);
+    }
+
+    @Test
+    public void should_statistics_accounts_by_date() {
+
+        List<AccountItem> accountItems = initAccountItems();
+        AccountsStatistics accountsStatistics = new AccountsStatistics();
+        List<AccountItem> accounts = accountsStatistics.statisticsAccountsBy(accountItems, "201902");
+        System.out.println(accounts);
+    }
+
+
 
 }
